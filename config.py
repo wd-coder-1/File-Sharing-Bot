3eadd560b48e1bdac089600a3f22300dbee3587c
WD CODER <white8devil8industries@gmail.com>
2025-04-06 22:18:42 +0530

    config.py

--- config.py
+++ config.py
@@ -30,10 +30,10 @@ DB_URI = os.environ.get("mongodb+srv://filesharebot1:Wdl1fD58HKZWm3jA@cluster0.b
 DB_NAME = os.environ.get("filesharebot1", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0")
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4")
 
 #start message
 START_PIC = os.environ.get("START_PIC","")
